8a1d54f9d2f5ecee857701669ff0a97cc5187674
David Cornelius <davidcornelius15@gmail.com>
2017-07-28 21:29:00 +0700

    Initial Commit

---
+++ tabbedApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tabbedApp
+//
+//  Created by Vesperia on 7/28/17.
+//  Copyright © 2017 Vesperia. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
